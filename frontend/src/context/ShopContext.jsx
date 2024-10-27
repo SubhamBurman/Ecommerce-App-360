9ca588da096df8162d14d1825ca557fe52eff92e
Subham Burman <subhamburman05@gmail.com>
2024-10-27 22:58:25 +0530

    3:56:41

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,17 +1,65 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) =>{
 
-    const currency = '$';
+    const currency = 'Rs';
     const delivery_fee = 10;
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false)
+    const [cartItems, setCartItems] = useState({})
+
+    const addToCart = async (itemID,size) => {
+
+        if(!size){
+            toast.error('Select Size')
+            return;
+        }
+
+        let cartData = structuredClone(cartItems);
+
+        if (cartData[itemID]) {
+            if (cartData[itemID][size]){
+                cartData[itemID][size] += 1;
+                
+            }
+            else{
+                cartData[itemID,size] = 1;
+
+            }
+        }
+        else{
+            cartData[itemID] = {};
+            cartData[itemID][size] = 1;
+
+        }
+        setCartItems(cartData);
+    }
+
+    const getCartCount = ()=>{
+        let totalCount = 0;
+        for(const items in cartItems){
+            for(const item in cartItems[items]){
+                try {
+                    if(cartItems [items] [item]>0){
+                        totalCount += cartItems[items][item]
+                    }
+                } catch (error) {
+                    
+                }
+            }
+
+        }
+        return totalCount;
+    }
 
     const value = {
         products , currency , delivery_fee,
-        search, setSearch, showSearch, setShowSearch
+        search, setSearch, showSearch, setShowSearch,
+        cartItems,addToCart,
+        getCartCount 
     }
 
     return (
