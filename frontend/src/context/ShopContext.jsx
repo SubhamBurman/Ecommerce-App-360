7827ff29e4a1b6a12daa1db09ec8b593a6e19138
SubhamBurman <105504422+SubhamBurman@users.noreply.github.com>
2024-11-27 08:13:34 +0000

    Complete frontend

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) =>{
@@ -10,6 +11,7 @@ const ShopContextProvider = (props) =>{
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false)
     const [cartItems, setCartItems] = useState({})
+    const navigate = useNavigate();
 
     const addToCart = async (itemID,size) => {
 
@@ -56,6 +58,7 @@ const ShopContextProvider = (props) =>{
 
 
     const updateQuantity = async (itemID, size, quantity) => {
+
         let cartData = structuredClone(cartItems);
         
         cartData[itemID][size] = quantity;
@@ -67,9 +70,9 @@ const ShopContextProvider = (props) =>{
         let totalAmount = 0;
         for (const items in cartItems){
             let itemInfo = products.find((product)=> product._id === items);
-            for(const item in cartItems){
+            for(const item in cartItems[items]){
                 try {
-                    if (cartItems[item][items]>0){
+                    if (cartItems[items][item]>0){
                         totalAmount +=itemInfo.price * cartItems[items][item];
                     }
                 } catch (error) {
@@ -85,7 +88,7 @@ const ShopContextProvider = (props) =>{
         search, setSearch, showSearch, setShowSearch,
         cartItems,addToCart,
         getCartCount , updateQuantity,
-        getCartAmount
+        getCartAmount, navigate
     }
 
     return (
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsletterBox from '../components/NewsletterBox'
 
 const About = () => {
   return (
     <div>
-      
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1={'ABOUT'} text2={'US'}/>
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+        <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum deleniti officia omnis, pariatur delectus quisquam cupiditate consequuntur excepturi fugit dignissimos aspernatur provident a quae ratione ad. Architecto nulla qui asperiores commodi. Dolorem iure repudiandae voluptatum praesentium quaerat minus fugiat libero itaque ipsa voluptates accusamus, earum quibusdam officia, ea ipsum ratione optio laborum dolorum qui exercitationem explicabo? Obcaecati voluptate id rerum.</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut veritatis perspiciatis ullam id! Deserunt in possimus eius molestiae obcaecati numquam odit pariatur assumenda at fuga, architecto veniam aspernatur delectus incidunt exercitationem, voluptatem, praesentium unde dolores esse! Fugiat quibusdam placeat veritatis.</p>
+          <b className='text-gray-800'>Our Mission</b>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus ullam minima quia a saepe ea, dolorem ut corrupti aliquam nemo perspiciatis dolore ipsum voluptatem asperiores ex suscipit aut quisquam explicabo sunt temporibus assumenda amet doloribus similique ab. Necessitatibus, officia reiciendis enim quos suscipit, odio quod animi assumenda architecto eos consequatur.</p>
+        </div>
+      </div>
+
+      <div className='text-xl py-4'>
+          <Title text1={'WHY'} text2={'CHOOSE US'}/>
+      </div>
+
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+             <b>Quality Assurance:</b>
+             <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui excepturi libero distinctio, repellendus unde odio neque, vitae sed enim eum, consectetur officia aliquid. Impedit, omnis autem aperiam molestiae debitis porro!</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+             <b>Convenience:</b>
+             <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui excepturi libero distinctio, repellendus unde odio neque, vitae sed enim eum, consectetur officia aliquid. Impedit, omnis autem aperiam molestiae debitis porro!</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+             <b>Exceptional Customer Service:</b>
+             <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui excepturi libero distinctio, repellendus unde odio neque, vitae sed enim eum, consectetur officia aliquid. Impedit, omnis autem aperiam molestiae debitis porro!</p>
+        </div>
+
+      </div>
+
+      <NewsletterBox/>
     </div>
   )
 }
